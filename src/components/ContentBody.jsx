fc24c36052ea65def631f7e4dc809708bd8bd0de
jesusjanm <janmjesus@gmail.com>
2020-05-28 01:13:29 -0430

    Extraccion de informacion de paises

--- src/components/ContentBody.jsx
+++ src/components/ContentBody.jsx
@@ -4,26 +4,72 @@ import './styles/ContentBody.css';
 
 class ContentBody extends React.Component {
 
+    state = {
+
+        countries: [],
+
+    };
+
     render(){
 
         return(
             <div className="containerGrid">
 
-                <div className="gridChildren">1</div>
-                <div className="gridChildren">2</div>
-                <div className="gridChildren">3</div>
-                <div className="gridChildren">4</div>
-                <div className="gridChildren">5</div>
-                <div className="gridChildren">6</div>
-                <div className="gridChildren">7</div>
-                <div className="gridChildren">8</div>
-                <div className="gridChildren">9</div>
+                    {this.state.countries.map( country => {
+
+                        return(
+                            <div className="gridChildren">
+                                <li key={country.alpha2Code} className="removeBulletPoint">
+                                    <label> {country.name} </label>
+                                    <img src={ country.flag } alt={ country.name } className="sizeFlag"/>
+                                </li>
+                            </div>
+                        );
+
+                    })}
+
 
             </div>
 
         )
 
     }
+
+    componentDidMount() {
+
+        console.log('El componente se termino de montar');
+
+        fetch('https://restcountries.eu/rest/v2/all/').then( (res) => {
+
+            if( res.ok ){
+                console.log('Peticion exitosa');
+            }
+            else {
+                console.log('Peticion no exitosa');
+            }
+
+            //console.log( `Peticion exitosa aqui esta la respuesta ${ JSON.stringify(res.json())}` );
+            return res.json();
+
+        }).then(( countries => {
+
+            console.log( `Estamos aqui ${countries}` );
+
+            this.setState( {
+
+                countries,
+
+            });
+
+        })).catch( err => {
+
+            console.log(`Ocurrio un error ${err}`);
+
+        });
+
+    }
+
+
 
 
 }
